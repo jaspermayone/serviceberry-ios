11c3e0e6eb33d159a494f519fc6c1fea3d86ad00
Jasper Mayone <me@jaspermayone.com>
2025-12-27 15:19:04 -0500

    Initial Commit

---
+++ serviceberry/serviceberryApp.swift
@@ -0,0 +1,17 @@
+//
+//  serviceberryApp.swift
+//  serviceberry
+//
+//  Created by Jasper Mayone on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct serviceberryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
